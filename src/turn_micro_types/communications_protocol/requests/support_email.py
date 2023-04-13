a606a14d9a8463d28adfb6b92c99d824d94503b3
daveturn <david@turning.ai>
2023-04-12 19:04:55 -0500

    updated InternalCommsReferencesPWPayload

--- src/turn_micro_types/communications_protocol/requests/support_email.py
+++ src/turn_micro_types/communications_protocol/requests/support_email.py
@@ -1,18 +1,18 @@
 from typing import Optional
 from uuid import UUID
 
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field, StrictInt, StrictStr
 from turn_micro_types.communications_protocol.constants import (
     SupportEmailCommunicationCategory,
 )
 
 
 class InternalCommsReferencesPWPayload(BaseModel):
-    pw_id: int
-    pw_uuid: Optional[UUID] = None
-    turn_id: str
-    partner_id: int
-    partner_name: str
+    pw_id: StrictInt
+    pw_uuid: UUID
+    turn_id: str = Field(regex="^C\\d{10}$")
+    partner_id: StrictInt
+    partner_name: StrictStr
     comm_type: SupportEmailCommunicationCategory
-    support_email: Optional[str] = None
-    info_for_agent: Optional[str] = None
+    support_email: Optional[EmailStr] = None
+    info_for_agent: Optional[StrictStr] = None
