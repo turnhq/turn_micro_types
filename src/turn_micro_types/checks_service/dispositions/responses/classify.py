19050cc0b8bd2a5a7551bd3007c9bc37982391c6
Samuel Reynoso Valle <samuel@turn.ai>
2024-06-17 12:28:20 -0600

    [SCREEN-4512] Dispositions - AI Classification

--- src/turn_micro_types/checks_service/dispositions/responses/classify.py
+++ src/turn_micro_types/checks_service/dispositions/responses/classify.py
@@ -1,18 +1,32 @@
-from typing import Dict
+from typing import Dict, Optional
 
 from pydantic import BaseModel
 
 
+class AiResponse(BaseModel):
+    disposition: Optional[str] = None
+    plea: Optional[str] = None
+    status: Optional[str] = None
+    model: Optional[str] = None
+    prompt: Optional[str] = None
+
+
 class ClassificationDataAttributes(BaseModel):
-    similarity: str
+    classification: str
+    source: str
+    ai_response: Optional[AiResponse] = None
+
+
+class ClassificationData(BaseModel):
     description: str
+    classification_data: ClassificationDataAttributes
 
 
 class ClassificationResponse(BaseModel):
     success: bool
-    data: ClassificationDataAttributes
+    data: ClassificationData
 
 
 class ClassificationDictResponse(BaseModel):
     success: bool
-    data: Dict[str, str]
+    data: Dict[str, ClassificationDataAttributes]
